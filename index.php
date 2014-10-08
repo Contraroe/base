5de23466a32887d96388e112d79a014163924538
Pieter De Norre <pieter.de.norre@telenet.be>
2014-10-08 10:32:29 +0200

    compass grunt etc

--- index.php
+++ index.php
@@ -1,27 +1,35 @@
 <!DOCTYPE html>
-<html>
-<head>
-	<meta http-equiv='content-type' content='text/html; charset=utf-8' />
-	<meta name='description' content='' />
-	<meta name='keywords' content='' />
-	<meta name='author' content='' />
-	<title>Title Website</title>
-	<link rel="stylesheet" href="css/reset.css" />
-	<link rel="stylesheet" href="css/index.css" />
-	<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
-	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.0.8/angular.min.js"></script>
-	<script type="text/javascript" src="js/live.js"></script>
-<body>
-	<div id="container">
-		<?php include 'includes/header.php' ?>
-	</div>
-	<div id="container">
-		<?php include 'includes/content.php' ?>
-	</div>
-	<div id="container">
-		<?php include 'includes/footer.php' ?>
-	</div>
-</body>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
 
+        <link rel="stylesheet" href="_css/normalize.css">
+        <link rel="stylesheet" href="_css/global.css">
+        <script src="_js/vendor/modernizr-2.6.2.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <!-- Add your site or application content here -->
+        <p>Hello world! This is HTML5 Boilerplate.</p>
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
+        <script src="_js/plugins.js"></script>
+        <script src="_js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script src="_js/vendor/google.analytics.js"></script>
+    </body>
 </html>
