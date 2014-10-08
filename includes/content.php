dad623ae1be8d46edd898be5471cc1712968d8bf
Pieter De Norre <pieter.de.norre@telenet.be>
2014-10-08 08:41:32 +0200

    basefiles

---
+++ includes/content.php
@@ -0,0 +1,3 @@
+<div ID="content">
+
+</div>
---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv='content-type' content='text/html; charset=utf-8' />
+	<meta name='description' content='' />
+	<meta name='keywords' content='' />
+	<meta name='author' content='' />
+	<title>Title Website</title>
+	<link rel="stylesheet" href="css/reset.css" />
+	<link rel="stylesheet" href="css/index.css" />
+	<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.0.8/angular.min.js"></script>
+	<script type="text/javascript" src="js/live.js"></script>
+<body>
+	<div id="container">
+		<?php include 'includes/header.php' ?>
+	</div>
+	<div id="container">
+		<?php include 'includes/content.php' ?>
+	</div>
+	<div id="container">
+		<?php include 'includes/footer.php' ?>
+	</div>
+</body>
+
+
+</html>
