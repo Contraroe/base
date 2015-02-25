707eb62dfff6eed91347f5955d4bd80779e37bf5
Pieter De Norre <pieter.de.norre@telenet.be>
2015-02-25 13:31:38 +0100

    v3.0
    susy grid system integration

--- index.php
+++ index.php
@@ -3,37 +3,66 @@
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title></title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title></title>
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+		<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
 
-        <link rel="stylesheet" href="_css/normalize.css">
-        <link rel="stylesheet" href="_css/global.css">
-        <script src="_js/vendor/modernizr-2.6.2.min.js"></script>
-    </head>
-    <body>
-        <!--[if lt IE 7]>
-            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-        <![endif]-->
+		<link rel="stylesheet" href="_css/normalize.css">
+		<link rel="stylesheet" href="_css/global.css">
+		<script src="_js/vendor/modernizr-2.6.2.min.js"></script>
+	</head>
+	<body>
+		<!--[if lt IE 7]>
+			<p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+		<![endif]-->
 
-        <!-- Add your site or application content here -->
-        <p>Start to grow</p>
-        <h1>Test</h1>
-        <h2>Test2</h2>
+		<!-- Add your site or application content here -->
 
-        <?php include '_includes/footer.php' ?>
+		<header class="grey">
 
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
-        <script src="_js/plugins.js"></script>
-        <script src="_js/main.js"></script>
 
-        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-        <script src="_js/vendor/google.analytics.js"></script>
-    </body>
+			<h1>Header content</h1>
+			<nav class="black">Nav</nav>
+
+
+		</header>
+		<section class="one">
+			<div class="content_1 grey nav">test</div>
+			<div class="content_2">test</div>
+			<div class="content_3 grey">test</div>
+			<div class="content_4">test</div>
+
+
+
+
+		</section>
+		<section class="two">
+
+			<div class="content_1 grey nav">test<br>test<br>test<br>test<br>test<br>test<br>test<br>test<br>test<br>test<br>test<br>test<br>test<br>test<br>test<br>test</div>
+			<div class="content_2">test</div>
+			<div class="content_3 grey ">test</div>
+			<div class="content_4">test</div>
+
+		</section>
+		<footer class="black">
+
+			Footer
+
+		</footer>
+
+
+
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+		<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
+		<script src="_js/plugins.js"></script>
+		<script src="_js/main.js"></script>
+
+		<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+		<script src="_js/vendor/google.analytics.js"></script>
+	</body>
 </html>
